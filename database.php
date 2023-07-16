044529117593df52b77d6e1222521abc88e1ef57
Syed Hussain Ahmed <106019684+HussainAhmed202@users.noreply.github.com>
2023-07-16 11:32:25 +0500

    uploaded database.php file
    This file establishes connection to our database.

---
+++ database.php
@@ -0,0 +1,25 @@
+<?php 
+    // This file is responsible for establishing connection to the database
+
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "project";
+    //$conn = "";
+
+
+    try {
+    // Create connection
+    $conn = mysqli_connect($db_server, $db_user,$db_pass,$db_name);
+
+    } 
+    catch (mysqli_sql_exception) {
+        echo "Could not connected to the database <br>";
+    }
+
+    if ($conn) {
+        echo "Connected successfully <br>";
+    }
+
+
+?>
