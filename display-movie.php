f539802e2835fa8301779a4f917d3ab6fcacbaf7
Syed Hussain Ahmed <106019684+HussainAhmed202@users.noreply.github.com>
2023-07-19 02:06:11 +0500

    final commit. removed echo

--- display-movie.php
+++ display-movie.php
@@ -1,6 +1,7 @@
-<?php //echo $_SERVER['PHP_SELF']; ?>
+<?php //echo $_SERVER['PHP_SELF']; 
+?>
 
-<?php 
+<?php
 session_start();
 ?>
 
@@ -9,18 +10,18 @@ $movie_id =  $_GET['id'];
 $name = $_SESSION['first'];
 ?>
 
-<?php 
-   include "database.php";
+<?php
+include "database.php";
 ?>
 
 
-<?php 
+<?php
 
 $sql = "SELECT * FROM movies where movie_id = {$movie_id}";
-$result = mysqli_query($conn,$sql);
+$result = mysqli_query($conn, $sql);
 $res = mysqli_fetch_assoc($result);
 
-
+// fetch data about selected movie from db
 $title = $res['title'];
 $release_year = $res['release_year'];
 $movie_rating = $res['movie_rating'];
@@ -34,9 +35,9 @@ $writers = $res['writers'];
 $movie_genre = $res['movie_genre_id'];
 $trailer = $res['trailer'];
 
-
+// get the genre
 $sql = "SELECT genre FROM genre where genre_id = {$movie_genre}";
-$result = mysqli_query($conn,$sql);
+$result = mysqli_query($conn, $sql);
 $res = mysqli_fetch_assoc($result);
 
 $genre = $res['genre'];
@@ -44,16 +45,17 @@ $genre = $res['genre'];
 
 
 <?php
-    $_SESSION['title'] = $title;
-    $_SESSION['movie_id'] = $movie_id;
+$_SESSION['title'] = $title;
+$_SESSION['movie_id'] = $movie_id;
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <title><?php echo $title; ?></title>
@@ -78,54 +80,45 @@ $genre = $res['genre'];
             <div class="search_user">
                 <input type="text" placeholder="search..." id="search_input">
                 <img src="https://api.dicebear.com/6.x/initials/svg?scale=100&size=32&seed=<?php echo $name ?>" alt="avatar" />
-                   <div class="search"> 
-                    <!-- <a href="#" class="card">
-                        <img src="img/the boys.jpg" alt="">
-                        <div class="cont">
-                            <h3>The Boys</h3>
-                            <p>Action, 2021 , <span>IMDB </span><i class="bi bi-star-fill"></i> 9.6</p>
-                        </div> 
-                    </a>     -->
-
-                </div>
-            </div>    
+            </div>
         </nav>1
         <div class="content">
             <h1 id="title" style="margin-top: 0px;"><?php echo $title ?></h1>
-            <p style="font-size: medium; margin-bottom: 13px;"><?php echo $plot; ?></p>
+            <p style="font-size: small; margin-bottom: 13px;"><?php echo $plot; ?></p>
 
-            <div class = 'additon_detail'> 
-                <h5 id="director" style="border-top-width: 0px;border-top-style: solid;border-bottom-width: 0px;border-bottom-style: solid;margin-top: 6px;margin-bottom: 10px;"><?php echo "Directed by ".$directors?></h5>
-                <h5 id="writer" style="margin-top: 10px;margin-bottom: 10px;"><?php echo "Written by ".$writers?></h5>
+            <div class='additon_detail'>
+                <h5 id="director" style="border-top-width: 0px;border-top-style: solid;border-bottom-width: 0px;border-bottom-style: solid;margin-top: 6px;margin-bottom: 10px;"><?php echo "Directed by " . $directors ?></h5>
+                <h5 id="writer" style="margin-top: 10px;margin-bottom: 10px;"><?php echo "Written by " . $writers ?></h5>
                 <h4 style="margin-top: 6px;margin-bottom: 6px;">Starring</h4>
-                <h5 id="casts" style="margin-top: 5px;margin-bottom: 0px;"><?php echo $casts?></h5>            
-            
-            <div class="details" style="margin-top: -13px;margin-bottom: -10px;">
-                <h6> <?php echo $certificate; ?></h6>
-                <h5 id = 'gen'><?php echo $genre; ?></h5>
-                <h4 id = 'date'><?php echo $release_year; ?></h4>
-                <h3 id="rate"><span>IMDB </span><i class="bi bi-star-fill"></i> <?php echo $movie_rating ?></p></h3>
+                <h5 id="casts" style="margin-top: 5px;margin-bottom: 0px;"><?php echo $casts ?></h5>
+
+                <div class="details" style="margin-top: -13px;margin-bottom: -10px;">
+                    <h6> <?php echo $certificate; ?></h6>
+                    <h5 id='gen'><?php echo $genre; ?></h5>
+                    <h4 id='date'><?php echo $release_year; ?></h4>
+                    <h3 id="rate"><span>IMDB </span><i class="bi bi-star-fill"></i> <?php echo $movie_rating ?></p>
+                    </h3>
+                </div>
+                <div class="btns">
+                    <a href="watch.php" id="play">WATCH <i class="bi bi-play-fill"></i></a>
+                    <a href="rate_movie.php">Rate Movie</a>
+
+                </div>
             </div>
-            <div class="btns">
-                <a href="watch.php" id="play">WATCH <i class="bi bi-play-fill"></i></a>                
-                <a href="rate_movie.php">Rate Movie</a>
-        
-            </div>
-        </div>
-        
+
     </header>
 </body>
-</html> 
+
+</html>
 
 
-<?php 
- // $conn : database connection object defined in database.php
+<?php
+// $conn : database connection object defined in database.php
 
- if (mysqli_ping($conn)) {
-    
+if (mysqli_ping($conn)) {
+
     // Connection is open so close it
     mysqli_close($conn);
-
- }
+}
 
 ?>
