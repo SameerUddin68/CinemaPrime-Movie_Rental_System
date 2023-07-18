2182e48132b69fcf53311cb09ba0bbe3fc773791
Syed Hussain Ahmed <106019684+HussainAhmed202@users.noreply.github.com>
2023-07-19 02:05:16 +0500

    final commit. removed echo

--- customer_signin.php
+++ customer_signin.php
@@ -1,81 +1,79 @@
-<?php echo "Welcome to " . $_SERVER["PHP_SELF"]; ?>
+<?php //echo "Welcome to " . $_SERVER["PHP_SELF"]; 
+?>
 
-<?php 
-   // establish connection to the database
-   include "database.php";
+<?php
+// establish connection to the database
+include "database.php";
 ?>
 
 
-<?php 
-   session_start();
+<?php
+session_start();
 ?>
 
 
-<?php 
+<?php
 
-   // display signin page
-   include "templates/customer_signin.html";
+// display signin page
+include "templates/customer_signin.html";
 
-   // $error stores any errors found. If empty then no errors found
-   $error = array();
+// $error stores any errors found. If empty then no errors found
+$error = array();
 
-   if (isset($_POST["signin"])) {
+if (isset($_POST["signin"])) {
 
-       // validate username
-       $username = filter_input(INPUT_POST,"username",FILTER_SANITIZE_SPECIAL_CHARS);        
-       if (!preg_match('/^(?![0-9])[\w]+$/i', $username)) {
-           array_push($error,"Invalid username");
-       }   
+    // validate username
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    if (!preg_match('/^(?![0-9])[\w]+$/i', $username)) {
+        array_push($error, "Invalid username");
+    }
 
-       // validate password
-       $password = filter_input(INPUT_POST,"password",FILTER_SANITIZE_SPECIAL_CHARS);
-       
-       if (empty($error)) {
-           // all data is valid and sanitized
+    // validate password
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
 
-           $username = ucwords(strtolower($username));
+    if (empty($error)) {
+        // all data is valid and sanitized
 
-           $sql = "SELECT password from customers where username = '$username' " ;
-           $result = mysqli_query($conn,$sql);
+        $username = ucwords(strtolower($username));
 
-           if (mysqli_num_rows($result) === 1 ) {
-               // User found
-               $row = mysqli_fetch_assoc($result);
+        $sql = "SELECT password from customers where username = '$username' ";
+        $result = mysqli_query($conn, $sql);
 
-               // get salted password stored in the database
-               $hash = $row['password'];
+        if (mysqli_num_rows($result) === 1) {
+            // User found
+            $row = mysqli_fetch_assoc($result);
 
-               if (password_verify($password,$hash)) {
-                   // valid user
-                   echo "Valid user";
+            // get salted password stored in the database
+            $hash = $row['password'];
 
-                   // store info about the user to be used further
-                   $_SESSION['username'] = $username;
-                   
-                   //Go to home page
-                   header("Location: home.php");
-                   exit();
-               }
-               else{
-                   // password mismatch
-                   echo "Invalid password";
-               }
-           }
-           else{
-               // username does not exist
-               echo "Invalid User";
-           }
-       }
-       else{
+            if (password_verify($password, $hash)) {
+                // valid user
+                echo "Valid user";
+
+                // store info about the user to be used further
+                $_SESSION['username'] = $username;
+
+                //Go to home page
+                header("Location: home.php");
+                exit();
+            } else {
+                // password mismatch
+                echo "Invalid password";
+            }
+        } else {
+            // username does not exist
+            echo "Invalid User";
+        }
+    } else {
         echo "Wrong input";
-       }
-   }
+    }
+}
 ?>
 
-<?php 
- // $conn : database connection object defined in database.php
- if (mysqli_ping($conn)) {
-   // Connection is open so close it
+<?php
+// $conn : database connection object defined in database.php
+if (mysqli_ping($conn)) {
+    // Connection is open so close it
 
-   mysqli_close($conn);
-}  
+    mysqli_close($conn);
+}
